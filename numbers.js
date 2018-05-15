bccb424afaff9d143f0de32ea550dfe999008ed4
Yayan <hilarymanuel@gmail.com>
2018-05-15 16:15:27 -0400

    exercies on numbers

---
+++ numbers.js
@@ -0,0 +1,18 @@
+let age = 39
+let dogYears = (age + 1) / 7
+
+console.log('Your age in dog years is: ' + dogYears.toFixed(2))
+
+// Challenge area
+
+// studentScore
+// maxScore
+// percent
+
+//print the percent
+
+let studentScore = 41
+let maxScore = 100
+let percent = (studentScore/maxScore) * 100
+
+console.log('Your grade is ' + percent.toFixed(2) + '%')
