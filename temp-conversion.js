525458834232cc55289b42be1e093b2d86146676
Yayan <hilarymanuel@gmail.com>
2018-05-16 15:26:25 -0400

    temperature conversion

---
+++ temp-conversion.js
@@ -0,0 +1,14 @@
+let fahrenheit = 32 //0C, 273.15K
+
+//calculate celsius and store in celsium variable
+// print that value
+
+//calculate kelvin value and store that value
+//print that value
+
+let celsius = ((fahrenheit-32)*5)/9
+let kelvin = celsius + 273.15
+
+console.log(fahrenheit + '\u00b0F' + ' = ' + celsius + '\u00b0C')
+console.log(fahrenheit + '\u00b0F' + ' = ' + kelvin + '\u00b0K')
+
